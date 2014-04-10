72d459152330cf9b613c55f5909d284bc8a8479a
stars <loinzaft@gmail.com>
2014-04-10 23:26:28 +0900

    initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,17 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-MonitoringDb::Application.config.secret_key_base = 'fa6f3428d96b19fbfae2c94be724e9fbf53a9437875c5debc483f3852cf641e1c2f5f986293d8a8134f847e38f9b3d29cc19e53a10d20011d9abb37cf60dd394'
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		File.read(token_file).chomp
+	else
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+MonitoringDb::Application.config.secret_key_base = secure_token
+
+#MonitoringDb::Application.config.secret_key_base = 'fa6f3428d96b19fbfae2c94be724e9fbf53a9437875c5debc483f3852cf641e1c2f5f986293d8a8134f847e38f9b3d29cc19e53a10d20011d9abb37cf60dd394'
