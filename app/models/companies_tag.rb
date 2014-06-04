a8e357361c23aa07216bdb0c3a87426761821bc7
stars <loinzaft@gmail.com>
2014-06-05 08:53:23 +0900

    201406050853

--- app/models/companies_tag.rb
+++
@@ -1,6 +0,0 @@
-class CompaniesTag < ActiveRecord::Base
-	belongs_to :company, class_name: "Company"
-	belongs_to :tag, class_name: "Tag"
-	validates :company_id, presence: true
-	validates :tag_id, presence: true
-end
