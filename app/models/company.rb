334720ec01ba5c87b2e99048e7d22db470435721
stars <loinzaft@gmail.com>
2014-12-29 17:36:21 +0900

    201412291736 ready to commit

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,7 +10,7 @@ class Company < ActiveRecord::Base
   has_many :links
   has_many :news_articles
 
-  #PaperClipの:logo_image設定
+  #PaperClipの:logo_image設定.
   has_attached_file :logo_image, styles: {medium: "300x300>", thumb: "100x100>"},
     :url => "/assets/logo_images/:id/:style/:basename.:extension",
     :path => ":rails_root/public/assets/logo_images/:id/:style/:basename.:extension"
